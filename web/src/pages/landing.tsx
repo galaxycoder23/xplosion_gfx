8f96d80c0f78108e7e8db87df6b2bdb7c021ab8b
hxs516 <145396594+galaxycoder23@users.noreply.github.com>
2025-11-06 20:58:57 +0000

    chore: fix typos on landing page

--- web/src/pages/landing.tsx
+++ web/src/pages/landing.tsx
@@ -2,13 +2,13 @@ export default function Landing(){
 
     return(<div className="m-5">
         <div>
-            <a className="text-lg font-bold underline" href="/lt">Lower Thrid Graphic</a>
+            <a className="text-lg font-bold underline" href="/lt">Lower Third Graphic</a>
         </div>
         <div>
-            <a className="text-lg font-bold underline" href="/lt_netball">Lower Thrid Graphic Netball</a>
+            <a className="text-lg font-bold underline" href="/lt_netball">Lower Third Graphic Netball</a>
         </div>
         <div>
-            <a className="text-lg font-bold underline" href="/lt_control">Lower Thrid Control</a>
+            <a className="text-lg font-bold underline" href="/lt_control">Lower Third Control</a>
         </div>
         <div>
             <a className="text-lg font-bold underline" href="/graph">Graph Graphic</a>
